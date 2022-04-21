108209a004da8efe8da3c4dba02ced1a045976db
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-04-21 01:44:57 +0000

    Add comment.php

---
+++ public/lab3/comment.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        // this is a comment
+        // echo "X equals $x";
+        $x += 10; // increment $x by 10
+        echo $x;
+        /* This is a section
+        of multiline comments
+        that will not be
+        interpreted */
+        ?>
+    </body>
+</html>
