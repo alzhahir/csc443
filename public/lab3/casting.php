8ba206cd64a91adb3985f2ac2336dfbd8896a058
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-04-21 02:18:21 +0000

    Add casting.php

---
+++ public/lab3/casting.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Basic PHP Syntax
+        </title>
+    </head>
+    <body>
+        <?php
+        $int = 12;
+        $double = 10.37;
+        $int = $int + $double;
+        echo $int;
+        ?>
+    </body>
+</html>
