7bba255abceed7107c7a5720b3ad00245cfedf7d
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-04-28 22:48:25 +0000

    Add form validation

--- public/lab4/handle_form.php
+++ public/lab4/handle_form.php
@@ -6,11 +6,50 @@
     <body>
         <?php #Script handle form
 
-            echo "<p> Thank you <b>".$_POST["name"].
-            "</b> for the following comments: <br/>".
-            $_POST["comments"]."<p>We will reply to you at <i>".
-            $_POST["email"]."</i></p>";
-            
+            if (strlen($_POST["name"]) > 0) {
+                $_POST["name"] = $_POST["name"];
+            } else {
+                $_POST["name"] = null;
+
+                echo "<p><b> You forgot to enter your name! </b></p>";
+            }
+
+            if (strlen($_POST["comments"]) > 0) {
+                $_POST["comments"] = $_POST["comments"];
+            } else {
+                $_POST["comments"] = null;
+
+                echo "<p><b> You forgot to enter your comment! </b></p>";
+            }
+
+            if (strlen($_POST["email"]) > 0) {
+                $_POST["email"] = $_POST["email"];
+            } else {
+                $_POST["email"] = null;
+
+                echo "<p><b> You forgot to enter your email! </b></p>";
+            }
+
+            if (isset($_POST["gender"])) {
+                if($_POST["gender"] == 'M'){
+                    $message = '<p><b>Good Day, Sir</b></p>';
+                }
+                if($_POST["gender"] == 'F'){
+                    $message = '<p><b>Good Day, Madam</b></p>';
+                }
+            } else {
+                $_POST["gender"] = null;
+
+                echo "<p><b> You forgot to choose your gender! </b></p>";
+            }
+
+            if($_POST["name"] && $_POST["comments"] && $_POST["email"] && $_POST["gender"]) {
+                echo "Thank you, ".$_POST["name"]. "for the following comment: <br/> <tt>".
+                $_POST["comments"]. "</tt> <p> We will reply to you at <i>" .$_POST["email"].
+                "</i><p>";
+                echo $message;
+            }
+
         ?>
     </body>
 </html>
