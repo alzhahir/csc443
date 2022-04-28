25774e321d7db8f8422f8a1b6ea3d5ce10380d84
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-04-28 23:37:31 +0000

    Add mainpage.php

---
+++ public/lab4/mainpage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Main Page</title>
+    </head>
+    <body>
+        <?php include("header.inc"); ?>
+        <br><br>
+        <a href="secondpage.php">Go to second page</a>
+    </body>
+</html>
