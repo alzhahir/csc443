06c29b9cdf741ba19b09a912e40c80bab2a0e038
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-05-12 01:12:43 +0000

    Add assocArray.php

---
+++ public/lab5/assocArray.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Basic PHP Syntax
+        </title>
+    </head>
+    <body>
+        <?php
+            $monthDays = array ('January' => 31, 'February' => 28,
+                                'March' => 31, 'April' => 30,
+                                'May' => 31, 'June' => 30,
+                                'July' => 31, 'August' => 31,
+                                'September' => 30, 'October' => 31,
+                                'November' => 30, 'December' => 31);
+            echo "<table border='1'><tr><th>Month</th><th>Days</th></tr>";
+            foreach($monthDays as $x => $xval){
+                echo "<tr><td>".$x."</td><td>".$xval."</td></tr>";
+            }
+            echo "</table>";
+        ?>
+    </body>
+</html>
