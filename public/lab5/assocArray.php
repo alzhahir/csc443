b962dbf402a40c1675da507afd55aed6f9ca80bc
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-05-12 06:30:29 +0000

    Display both all months and months with 30 days

--- public/lab5/assocArray.php
+++ public/lab5/assocArray.php
@@ -13,13 +13,18 @@
                                 'July' => 31, 'August' => 31,
                                 'September' => 30, 'October' => 31,
                                 'November' => 30, 'December' => 31);
-            echo "<table border='1'><tr><th>Month</th><th>Days</th></tr>";
+            $month30 = array();
+            echo "<h2>Months around the year</h2><table border='1'><tr><th>Month</th><th>Days</th></tr>";
             foreach($monthDays as $x => $xval){
+                echo "<tr><td>".$x."</td><td>".$xval."</td></tr>";
                 if($xval == 30){
-                    echo "<tr><td>".$x."</td><td>".$xval."</td></tr>";
+                    array_push($month30, $x);
                 }
             }
-            echo "</table>";
+            echo "</table><br><br><h2>Months with 30 days</h2>";
+            foreach($month30 as $v){
+                echo $v."<br>";
+            }
         ?>
     </body>
 </html>
