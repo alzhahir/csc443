56a8c75287a1fa86a7ad9ffe61cf208bc1d3a2ea
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-05-12 01:03:53 +0000

    Change code to sort alphabetically

--- public/lab5/indexedArray.php
+++ public/lab5/indexedArray.php
@@ -10,6 +10,7 @@
             $month = array ('January', 'February', 'March', 'April',
                             'May', 'June', 'July', 'August', 'September',
                             'October', 'November', 'December');
+            sort($month);
             for($i = 0; $i < 12; $i++){
                 echo $month[$i], "\t";
             };
