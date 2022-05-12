f6c0b889cdf536f47841fd4223d69def03d7a180
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-05-12 05:41:09 +0000

    Display both original array and sorted array

--- public/lab5/indexedArray.php
+++ public/lab5/indexedArray.php
@@ -10,10 +10,14 @@
             $month = array ('January', 'February', 'March', 'April',
                             'May', 'June', 'July', 'August', 'September',
                             'October', 'November', 'December');
-            sort($month);
             for($i = 0; $i < 12; $i++){
                 echo $month[$i], "\t";
             };
+            echo "<br><br>";
+            sort($month, SORT_STRING);
+            foreach($month as $x){
+                echo $x, "<br>";
+            }
         ?>
     </body>
 </html>
