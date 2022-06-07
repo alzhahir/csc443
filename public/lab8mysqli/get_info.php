d4b282a365b9b1de4a745e9beed095fb3db24c28
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-07 02:07:04 +0000

    Add email validation

--- public/lab8mysqli/get_info.php
+++ public/lab8mysqli/get_info.php
@@ -3,7 +3,7 @@
         <form action="add_user.php" method="post">
             First Name: <input type="text"  name="firstname" required><br>
             Lastname: <input type="text" name="firstname" required><br>
-            E-mail: <input type="text" name="email" required><br>
+            E-mail: <input type="email" name="email" required><br>
             Password: <input type="text" name="password" required><br>
             Registration date: <input type="date" name="reg_date" required><br>
             <input type="submit" value="Submit Data">
