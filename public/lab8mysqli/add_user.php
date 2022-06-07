3a94cf62a22b4ee566cbe42e9a02dedc8f4d1de9
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-07 01:47:24 +0000

    Change require_once back to include

--- public/lab8mysqli/add_user.php
+++ public/lab8mysqli/add_user.php
@@ -1,10 +1,10 @@
 <?php
-    require_once 'conn.php';
+    include 'conn.php';
 
     $sql = "INSERT INTO user (first_name, last_name, email, pass_word, registration_date) values ('$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[password]', '$_POST[reg_date]')";
     if(!mysqli_query($conn, $sql)){
         die('Error: '.mysqli_error($conn));
     }
     echo "1 record successfully added...";
-    header( "refresh:5;url=display_table.php" );
+    header("refresh:5; url=display_table.php");
 ?>
