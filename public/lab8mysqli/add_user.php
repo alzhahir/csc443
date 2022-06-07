ddd11250ec74cb4495ca852bbed2ee5ef8e27d91
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-07 01:40:09 +0000

    Fix bugs

--- public/lab8mysqli/add_user.php
+++ public/lab8mysqli/add_user.php
@@ -1,9 +1,10 @@
 <?php
-    include 'conn.php';
+    require_once 'conn.php';
 
-    $sql = "INSERT INTO user (first_name, last_name, email, pass_word) values ('$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[password]', '$_POST[reg_date]')";
+    $sql = "INSERT INTO user (first_name, last_name, email, pass_word, registration_date) values ('$_POST[firstname]', '$_POST[lastname]', '$_POST[email]', '$_POST[password]', '$_POST[reg_date]')";
     if(!mysqli_query($conn, $sql)){
         die('Error: '.mysqli_error($conn));
     }
     echo "1 record successfully added...";
+    header( "refresh:5;url=display_table.php" );
 ?>
--- public/lab8mysqli/conn.php
+++ public/lab8mysqli/conn.php
@@ -12,6 +12,7 @@
     define('DB_PASSWORD', $password);
     define('DB_NAME', $database);
 
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     if (mysqli_connect_errno())
     {
         echo "Failed to connect to MySQL: ".mysqli_connect_error();
