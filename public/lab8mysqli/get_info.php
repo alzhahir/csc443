2a56ba258df0aa3b1636e6d0293f6e704debeaa1
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-07 01:10:19 +0000

    Add get_info.php

---
+++ public/lab8mysqli/get_info.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form action="add_user.php" method="post">
+            First Name: <input type="text"  name="firstname" required><br>
+            Lastname: <input type="text" name="firstname" required><br>
+            E-mail: <input type="text" name="email" required><br>
+            Password: <input type="text" name="password" required><br>
+            Registration date: <input type="date" name="reg_date" required><br>
+            <input type="submit" value="Submit Data">
+        </form>
+    </body>
+</html>
