da661e4055735d6e8e2e0c86fc5faa1d56dc35ac
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-08 09:03:22 +0000

    Change line break

--- public/lab9/next_page.php
+++ public/lab9/next_page.php
@@ -3,10 +3,10 @@
     echo "<html>";
     
     $myLogin = $_SESSION["myLogin"];
-    echo "Value of myLogin has been retrieved: ".$myLogin."\n";
+    echo "Value of myLogin has been retrieved: ".$myLogin."<br>";
 
     $myColor = $_SESSION["myColor"];
-    echo "Value of myColor has been retrieved: ".$myColor."\n";
+    echo "Value of myColor has been retrieved: ".$myColor."<br>";
 
     echo "</html>\n";
 ?>
